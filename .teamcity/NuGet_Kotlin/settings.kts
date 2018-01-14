32c66b2c038cadf60f92ace3c5a73a6ff09149f3
michael dudley <michael.dudley@improvingenterprises.com>
2018-01-14 17:56:19 -0600

    trying to create a new project

---
+++ .teamcity/NuGet_Kotlin/settings.kts
@@ -0,0 +1,11 @@
+package NuGet_Kotlin
+
+import jetbrains.buildServer.configs.kotlin.v2017_2.*
+
+version = "2017.2"
+project(Project({
+    uuid = "f34ab715-ae6a-4625-868d-1308b5acd373"
+    parentId = "NuGet"
+    id   = "NuGet_Kotlin"
+    name = "Kotlin Builds"
+}))
